21d2b5fd0fb6fb302c6923d82ac0e1c370b26cdd
worldprog21 <prog.world21@gmail.com>
2025-05-16 15:04:57 +0330

    showing products images

--- app/pos/page.jsx
+++ app/pos/page.jsx
@@ -45,6 +45,8 @@ export default function POS() {
     try {
       const params = new URLSearchParams();
 
+      params.append("populate[0]", "image");
+
       if (debouncedSearch) {
         params.append("filters[name][$containsi]", debouncedSearch);
       }
@@ -200,7 +202,10 @@ export default function POS() {
                 className="w-full cursor-pointer overflow-hidden rounded-lg border border-primary shadow-sm p-0 hover:opacity-80"
               >
                 <img
-                  src="/product.png"
+                  src={
+                    process.env.NEXT_PUBLIC_STRAPI_URL + product.image.url ||
+                    "/product.png"
+                  }
                   alt="Product Image"
                   width={600}
                   height={400}

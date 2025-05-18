2409b6f481d490a2f45411bccd18e38026b9197a
worldprog21 <prog.world21@gmail.com>
2025-05-18 19:22:51 +0330

    updating next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [new URL("http://localhost:1337/**")],
+    remotePatterns: [
+      new URL("http://localhost:1337/**"),
+      new URL("https://inventory-backend-57oe.onrender.com/**"),
+    ],
   },
 };
 
